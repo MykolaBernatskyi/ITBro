f1f8f1a94d590b33a14aa904ef53d5573d306add
Mykola Bernatskyi <bernatskyi.mykola@gmail.com>
2022-01-05 01:11:32 +0200

    add img...

--- JavaScript/HW_02 DOM data types/starter/app.js
+++ JavaScript/HW_02 DOM data types/starter/app.js
@@ -4,7 +4,7 @@ let scores, roundScore, activePlayer, dice;
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random() * 6) + 1;
+
 
 // Усе можна використовувати, але усе має свої особливості!!!
 // innerHTML - фактично працює з HTML елементами
@@ -12,6 +12,28 @@ dice = Math.floor(Math.random() * 6) + 1;
 // innerText - може считувати стилі, але не буде виводити приховані елементи!
 // document.querySelector('#current-' + activePlayer).innerText = dice;
 // textContent - отримує значення усіх елементів, включаючи скріпти, стилі: можемо отримувати та передавати певні значення
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+
+// Можна працювати з даними (зчитувати їх)
+// let x = document.querySelector('#score-0').textContent;
+//console.log(x);
 
 
+// працюємо з кубиком, але для початку ми його приховаємо
+document.querySelector('.dice').style.display = 'none';
+
+//
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+
+
+
+//працюємо з кнопкою, яка запускає кидання кубика
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    dice = Math.floor(Math.random() * 6) + 1;
+    let diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + ".png";
+});
